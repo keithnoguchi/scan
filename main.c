7d6bfc87a6ba40eae5e2d46b943f2f49b212afbb
Kei Nohguchi <kei@nohguchi.com>
2016-06-06 15:52:56 -0700

    main.c: struct scanner to host the scanner attribute

--- main.c
+++ main.c
@@ -2,18 +2,50 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "utils.h"
 
+struct scanner {
+	int family;
+	int type;
+	int protocol;
+	int fd;
+};
+
+void init(struct scanner *sc)
+{
+	sc->fd = socket(sc->family, sc->type, sc->protocol);
+	if (sc->fd == -1)
+		fatal("socket(2)");
+}
+
+void term(struct scanner *sc)
+{
+	if (sc->fd != -1)
+		close(sc->fd);
+	sc->fd = -1;
+}
+
 int main(int argc, char *argv[])
 {
-	int fd;
+	struct scanner sc;
+	int sock;
+	int ev;
 
-	fd = epoll_create1(0);
-	if (fd != -1)
+	/* This will be set through the command line. */
+	sc.family = AF_INET;
+	sc.type = SOCK_STREAM;
+	sc.protocol = IPPROTO_TCP;
+	init(&sc);
+
+	ev = epoll_create1(0);
+	if (ev == -1)
 		fatal("epoll_create1(2)");
 
-	close(fd);
+	close(ev);
+	term(&sc);
 
 	exit(EXIT_SUCCESS);
 }
