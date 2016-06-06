1d2ab503a8a902ecb7718b91424b44b736e3a31e
Kei Nohguchi <kei@nohguchi.com>
2016-06-06 15:59:20 -0700

    main.c: Register raw socket through epoll_ctl(2)

--- main.c
+++ main.c
@@ -30,9 +30,11 @@ void term(struct scanner *sc)
 
 int main(int argc, char *argv[])
 {
+#define MAX_EVENTS 10
+	struct epoll_event ev, events[MAX_EVENTS];
 	struct scanner sc;
-	int sock;
-	int ev;
+	int ret;
+	int fd;
 
 	/* This will be set through the command line. */
 	sc.family = AF_INET;
@@ -40,11 +42,17 @@ int main(int argc, char *argv[])
 	sc.protocol = IPPROTO_TCP;
 	init(&sc);
 
-	ev = epoll_create1(0);
-	if (ev == -1)
+	fd = epoll_create1(0);
+	if (fd == -1)
 		fatal("epoll_create1(2)");
 
-	close(ev);
+	ev.events = EPOLLIN;
+	ev.data.fd = sc.fd;
+	ret = epoll_ctl(fd, EPOLL_CTL_ADD, sc.fd, &ev);
+	if (ret == -1)
+		fatal("epoll_ctl(2)");
+
+	close(fd);
 	term(&sc);
 
 	exit(EXIT_SUCCESS);
