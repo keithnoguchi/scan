baecf8d9036cf5cb8144e5bac07d22c9f258419e
Kei Nohguchi <kei@nohguchi.com>
2016-06-06 23:47:25 -0700

    main.c: Complete writing once it come to the end port number

--- main.c
+++ main.c
@@ -7,43 +7,72 @@
 
 #include "utils.h"
 
+static const int start_port = 0;
+static const int end_port = 65535;
+
 struct scanner {
+	int eventfd;
 	int fd;
 	int family;
-	int type;
-	int protocol;
+	int proto;
+	int next_port;
+	int start_port;
+	int end_port;
 	int (*reader)(struct scanner *sc);
 	int (*writer)(struct scanner *sc);
 };
+
+static int __reader(struct scanner *sc)
+{
+	printf("reader\n");
+	return 0;
+}
+
+static int __writer(struct scanner *sc)
+{
+	if (++sc->next_port > sc->end_port) {
+		struct epoll_event ev;
+
+		ev.events = EPOLLIN;
+		ev.data.fd = sc->fd;
+		epoll_ctl(sc->eventfd, EPOLL_CTL_MOD, sc->fd, &ev);
+		printf("done with sending\n");
+	}
+	return 0;
+}
 
 void init(struct scanner *sc, int eventfd, int family, int proto)
 {
 	struct epoll_event ev;
 	int ret;
 
+	sc->eventfd = eventfd;
 	sc->family = family;
-	sc->protocol = proto;
-	sc->fd = socket(sc->family, SOCK_RAW, sc->protocol);
+	sc->proto = proto;
+	sc->fd = socket(sc->family, SOCK_RAW, sc->proto);
 	if (sc->fd == -1)
 		fatal("socket(2)");
 
 	/* We'll set this later. */
-	sc->reader = NULL;
-	sc->writer = NULL;
+	sc->start_port = start_port;
+	sc->end_port = end_port;
+	sc->next_port = sc->start_port;
+	sc->reader = __reader;
+	sc->writer = __writer;
 
-	/* Register to the event manager. */
+	/* Register it to the event manager. */
 	ev.events = EPOLLIN|EPOLLOUT;
 	ev.data.fd = sc->fd;
 	ev.data.ptr = (void *)sc;
-	ret = epoll_ctl(eventfd, EPOLL_CTL_ADD, sc->fd, &ev);
+	ret = epoll_ctl(sc->eventfd, EPOLL_CTL_ADD, sc->fd, &ev);
 	if (ret == -1)
 		fatal("epoll_ctl(2)");
 }
 
-void term(struct scanner *sc, int eventfd)
+void term(struct scanner *sc)
 {
 	if (sc->fd != -1) {
-		epoll_ctl(eventfd, EPOLL_CTL_DEL, sc->fd, NULL);
+		epoll_ctl(sc->eventfd, EPOLL_CTL_DEL, sc->fd, NULL);
 		close(sc->fd);
 	}
 	sc->fd = -1;
@@ -89,7 +118,7 @@ int main(int argc, char *argv[])
 			writer(scp);
 	}
 
-	term(&sc, eventfd);
+	term(&sc);
 	close(eventfd);
 
 	exit(EXIT_SUCCESS);
