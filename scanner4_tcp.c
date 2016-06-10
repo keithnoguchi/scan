b4fef37ddb694748a10362a6e248d468ba58c7dd
Kei Nohguchi <kei@nohguchi.com>
2016-06-10 14:30:30 -0700

    scanner4_tcp.c: Use netinet header file
    This will in sync with the scanner6_tcp.c.

--- scanner4_tcp.c
+++ scanner4_tcp.c
@@ -2,10 +2,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <linux/ip.h>
-#include <linux/tcp.h>
 
 #include "utils.h"
 #include "scanner.h"
